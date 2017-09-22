1ec099c29f6776d135b989b283180c4ee77d29f3
Shuanglei Tao <tsl0922@gmail.com>
2017-09-23 00:20:03 +0800

    Update copyright year

--- src/main/java/net/coding/jenkins/plugin/webhook/WebHookEnvironmentContributor.java
+++ src/main/java/net/coding/jenkins/plugin/webhook/WebHookEnvironmentContributor.java
@@ -1,7 +1,7 @@
 /**
  * Jenkins plugin for Coding https://coding.net
  *
- * Copyright (C) 2016 Shuanglei Tao <tsl0922@gmail.com>
+ * Copyright (C) 2016-2017 Shuanglei Tao <tsl0922@gmail.com>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
